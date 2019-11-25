37310ff6c44a003fb128eb4b3a069fea8660b0f4
Siddhartha Chandra <siddharthac@support.com>
2019-11-25 02:14:38 -0800

    First checkin

--- server/config/database.js
+++ server/config/database.js
@@ -1,14 +1,14 @@
 module.exports = {
   development: {
     username: 'root',
-    password: 'root',
+    password: 'R00tr00t',
     database: 'ecommerce',
     host: '127.0.0.1',
     dialect: 'mysql'
   },
   test: {
     username: 'root',
-    password: 'root',
+    password: 'R00tr00t',
     database: 'boilerplate_test',
     host: '127.0.0.1',
     dialect: 'mysql',
