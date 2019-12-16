dc8b307827a8fd18a9d73a6c564a047c4c4a0655
Siddhartha Chandra <siddharthac@support.com>
2019-12-16 00:56:19 -0800

    Enabled cors for category api

--- server/src/interfaces/http/category/CategoriesController.js
+++ server/src/interfaces/http/category/CategoriesController.js
@@ -8,6 +8,11 @@ const CategoriesController = {
     const router = Router();
 
     router.use(inject("categorySerializer"));
+    router.use(function(req, res, next) {
+      res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      next();
+    });
 
     router.get("/", inject("getAllCategories"), this.index);
     router.get("/findByDepartment", inject("getAllCategoriesByDepartment"), this.getAllCategoriesByDepartment);
