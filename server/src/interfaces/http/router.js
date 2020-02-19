39f78eefba21e137a9e8e34d440ad73edd3e4e8d
kapilkarda <kkarda77@gmail.com>
2020-02-19 19:28:39 +0530

    router resolve

--- server/src/interfaces/http/router.js
+++ server/src/interfaces/http/router.js
@@ -9,10 +9,10 @@ const passport = require("../../../config/passport");
 var express = require('express');
 var app = express();
 
-const ngrok = require('ngrok');
-(async function() {
-  const url = await ngrok.connect();
-})();
+//const ngrok = require('ngrok');
+//(async function() {
+ // const url = await ngrok.connect();
+//})();
 module.exports = ({
   config,
   containerMiddleware,
