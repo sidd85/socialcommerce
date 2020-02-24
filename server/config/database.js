aa6e6b4eda49567e579b3dea1b268d8083bd76cb
kapilkarda <kkarda77@gmail.com>
2020-02-24 17:49:00 +0530

    Admin access

--- server/config/database.js
+++ server/config/database.js
@@ -1,15 +1,16 @@
+production: process.env.DATABASE_URL
 module.exports = {
   development: {
   username: 'root',
-  password: 'admin',
+  password: 'R00tr00t',
   database: 'ecommerce',
   host: '127.0.0.1',
-  port: '3306',
+  port: '3307',
   dialect: 'mysql'
   },
   test: {
   username: 'root',
-  password: 'admin',
+  password: 'R00tr00t',
   database: 'boilerplate_test',
   host: '127.0.0.1',
   dialect: 'mysql',
@@ -18,41 +19,11 @@ module.exports = {
   },
   production: {
   username: 'root',
-  password: 'admin',
+  password: 'R00tr00t',
   database: 'ecommerce',
   host: '127.0.0.1',
-  port: '3306',
+  port: '3307',
   dialect: 'mysql'
   },
   };
   
-//production: process.env.DATABASE_URL
-// module.exports = {
-//   development: {
-//   username: 'root',
-//   password: 'R00tr00t',
-//   database: 'ecommerce',
-//   host: '127.0.0.1',
-//   port: '3307',
-//   dialect: 'mysql'
-//   },
-//   test: {
-//   username: 'root',
-//   password: 'R00tr00t',
-//   database: 'boilerplate_test',
-//   host: '127.0.0.1',
-//   dialect: 'mysql',
-//   dialectModule: 'mysql2',
-//   logging: null
-//   },
-//   production: {
-//   username: 'root',
-//   password: 'R00tr00t',
-//   database: 'ecommerce',
-//   host: '127.0.0.1',
-//   port: '3307',
-//   dialect: 'mysql'
-//   },
-//   };
-  
-// //   //production: process.env.DATABASE_URL
