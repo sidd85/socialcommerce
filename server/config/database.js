4f0bee8e062578d75c2cf1f019fab4a1a5bea08e
kapilkarda <kkarda77@gmail.com>
2020-02-27 11:10:03 +0530

    database server

--- server/config/database.js
+++ server/config/database.js
@@ -1,59 +1,27 @@
 module.exports = {
   development: {
-    username: 'root',
-    password: 'admin',
-    database: 'ecommerce',
-    host: '127.0.0.1',
-    port: '3306',
-    dialect: 'mysql'
+  username: 'root',
+  password: 'R00tr00t',
+  database: 'ecommerce',
+  host: '127.0.0.1',
+  port: '3307',
+  dialect: 'mysql'
   },
   test: {
-    username: 'root',
-    password: 'admin',
-    database: 'boilerplate_test',
-    host: '127.0.0.1',
-    dialect: 'mysql',
-    dialectModule: 'mysql2',
-    logging: null
+  username: 'root',
+  password: 'R00tr00t',
+  database: 'boilerplate_test',
+  host: '127.0.0.1',
+  dialect: 'mysql',
+  dialectModule: 'mysql2',
+  logging: null
   },
   production: {
-    username: 'root',
-    password: 'admin',
-    database: 'ecommerce',
-    host: '127.0.0.1',
-    port: '3306',
-    dialect: 'mysql'
+  username: 'root',
+  password: 'R00tr00t',
+  database: 'ecommerce',
+  host: '127.0.0.1',
+  port: '3307',
+  dialect: 'mysql'
   },
-}
-
-// module.exports = {
-//   development: {
-//   username: 'root',
-//   password: 'R00tr00t',
-//   database: 'ecommerce',
-//   host: '127.0.0.1',
-//   port: '3307',
-//   dialect: 'mysql'
-//   },
-//   test: {
-//   username: 'root',
-//   password: 'R00tr00t',
-//   database: 'boilerplate_test',
-//   host: '127.0.0.1',
-//   dialect: 'mysql',
-//   dialectModule: 'mysql2',
-//   logging: null
-//   },
-//   production: {
-//   username: 'root',
-//   password: 'R00tr00t',
-//   database: 'ecommerce',
-//   host: '127.0.0.1',
-//   port: '3307',
-//   dialect: 'mysql'
-//   },
-//   };
-  
-// // production: process.env.DATABASE_URL
-
-
+  };
